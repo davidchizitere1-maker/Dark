772f8861c1c243be58a804837f0b80446168c6ac
Dark <davidchizitere1@gmail.com>
2026-09-25 00:04:59 +0100

    Delete games/board-games/stick-ops directory

--- games/board-games/stick-ops/src/data/README.txt
+++
@@ -1,1 +0,0 @@
-Weapon and enemy definitions live in src/config.js so the build remains data-driven. Add new profiles there without changing the core loop.
--- games/board-games/stick-ops/src/entities/projectile.js
+++
@@ -1,7 +0,0 @@
-export class Projectile{
-  constructor({x,y,vx,vy,damage,owner,weaponId,color,radius=4,pierce=0,explosive=false}){
-    Object.assign(this,{x,y,vx,vy,damage,owner,weaponId,color,radius,pierce,explosive});
-    this.life=2.2;this.dead=false;this.age=0;this.hitIds=new Set();this.gravity=explosive?260:0;
-  }
-  update(dt,scale=1){this.age+=dt*scale;this.x+=this.vx*dt*scale;this.y+=this.vy*dt*scale;this.vy+=this.gravity*dt*scale;this.life-=dt*scale;if(this.life<=0)this.dead=true}
-}
